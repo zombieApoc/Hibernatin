c00bc49d401158f7dec237f7cea8b13b20295b48
Michael Grill <Ultramar@michaels-mbp.apollosrc.root>
2016-05-02 15:30:36 -0400

    Startpoint

---
+++ Hibernatin/src/main/java/com/theironyard/clt/HibernatinApplication.java
@@ -0,0 +1,12 @@
+package com.theironyard.clt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernatinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibernatinApplication.class, args);
+	}
+}
